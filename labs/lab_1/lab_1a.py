a06204213e2d9e128b93152d4461ac219e38650e
LierbagonGit <gabriel.vialle@outlook.com>
2026-03-28 19:51:40 -0400

    Added my intro

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Gabriel" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hi all! My name is Gabriel, and I am a Junior at Newton South High School. I have lived in France, England and Mauritius, a tropical island off the coast of Madagascar. I am interested in engineering and try to take all the engineering courses I can fit into my schedule. Aeronautical engineering is my main interest, and I wish to study it, which explains why I chose UAV course. \nI am looking forward to meeting and working with you!")
 if __name__ == "__main__":
     main()
