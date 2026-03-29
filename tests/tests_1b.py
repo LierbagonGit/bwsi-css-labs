387d5aeb88dbe12136939ee5aaec85b3c87019f0
LierbagonGit <gabriel.vialle@outlook.com>
2026-03-29 12:58:58 -0400

    More long lines...

--- tests/tests_1b.py
+++ tests/tests_1b.py
@@ -34,9 +34,23 @@ def test_division_by_zero():
         simple_calculator("divide", 5, 0)               # Test division by zero
 
 def test_invalid_operation():
-    with pytest.raises(ValueError, match="Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'."):
+    with pytest.raises(
+    ValueError,
+    match=(
+        "Invalid operation. Please choose from 'add', 'subtract', "
+        "'multiply', or 'divide'."
+    ),
+    ):
+
         simple_calculator("modulus", 5, 3)              # Test for invalid operation
-    with pytest.raises(ValueError, match="Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'."):
+    with pytest.raises(
+    ValueError,
+    match=(
+        "Invalid operation. Please choose from 'add', 'subtract', "
+        "'multiply', or 'divide'."
+    ),
+    ):
+
         simple_calculator("", 5, 3)                     # Test for empty operation
 
 if __name__ == "__main__":
